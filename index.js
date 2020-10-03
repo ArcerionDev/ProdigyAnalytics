641cf1b1167b1167063e04d03840066a2795f7fd
Arcerion <60945964+Avn1114@users.noreply.github.com>
2020-10-03 15:22:01 -0400

    more commands, FINALLY fixed daily graph cmnd

--- index.js
+++ index.js
@@ -17,11 +17,6 @@ setInterval(async function () {
     let hourstats = [hour + ':00', playerCounts.reduce((a, b) => a + b)]
     houranalytics.push(hourstats)
 }, 3600000)
-setInterval(function(){
-    while (houranalytics > 12) {
-        houranalytics.pop()
-    }
-},)
 bot.on('ready', () => {
     console.log('Online');
     bot.user.setActivity('p:traffic', {
@@ -30,6 +25,9 @@ bot.on('ready', () => {
 
 })
 bot.on('message', async message => {
+    if (houranalytics.length > 12) {
+        houranalytics.length = 12
+    }
     let args = message.content.split(' ')
     switch (args[0]) {
         case 'p:traffic':
@@ -69,14 +67,14 @@ bot.on('message', async message => {
                         data: { labels: timestamps, datasets: [{ label: 'Traffic', data: traffic }] },
                     })
                     .setWidth(800)
-                    .setHeight(400) 
+                    .setHeight(400)
                     .setBackgroundColor('transparent');
 
-                    const dailyEmb = new Discord.MessageEmbed()
-                    dailyEmb.setTitle('Here is the traffic over the last 12 hours.')
-                    dailyEmb.setImage(trafficDaily.getUrl())
+                const dailyEmb = new Discord.MessageEmbed()
+                dailyEmb.setTitle('Here is the traffic over the last 12 hours.')
+                dailyEmb.setImage(trafficDaily.getUrl())
                 message.channel.send(dailyEmb)
-                }
+            }
             break;
         case 'p:eval':
             args.shift()
@@ -99,6 +97,91 @@ bot.on('message', async message => {
             } catch (err) {
                 message.channel.send(` \`\`\`js\n${clean(err)}\n\`\`\``);
             }
-    }
+            break;
+        case 'p:avg':
+            let avgworlds = await (await fetch('https://api.prodigygame.com/multiplayer-api/worlds')).json()
+            let avgplayerCounts = []
+
+            for (i = 0; i < avgworlds.length; i++) {
+                avgplayerCounts.push(avgworlds[i].count)
+            }
+            const avgemb = new Discord.MessageEmbed()
+                .setTitle('Here is the average Prodigy traffic per world.')
+                .setDescription(`Currently an average of **${avgplayerCounts.reduce((a, b) => a + b) / avgworlds.length}** users are on per world out of **${avgworlds.length}** different worlds.`)
+                .setTimestamp()
+                .setFooter(`Requested by ${message.author.tag}.`)
+            message.channel.send(avgemb)
+            break;
+        case 'p:popular':
+            let popularworlds = await (await fetch('https://api.prodigygame.com/multiplayer-api/worlds')).json()
+            let popularplayerCounts = []
+
+            for (i = 0; i < popularworlds.length; i++) {
+                popularplayerCounts.push(popularworlds[i].count)
+            }
+            function findbiggest() {
+                let max = popularplayerCounts.reduce(function (a, b) {
+                    return Math.max(a, b);
+                })
+                return max;
+            };
+            let fourmax = []
+            fourmax.push(popularworlds[popularplayerCounts.indexOf(findbiggest())])
+            popularplayerCounts[popularplayerCounts.indexOf(findbiggest())] = 0
+            fourmax.push(popularworlds[popularplayerCounts.indexOf(findbiggest())])
+            popularplayerCounts[popularplayerCounts.indexOf(findbiggest())] = 0
+            fourmax.push(popularworlds[popularplayerCounts.indexOf(findbiggest())])
+            popularplayerCounts[popularplayerCounts.indexOf(findbiggest())] = 0
+            fourmax.push(popularworlds[popularplayerCounts.indexOf(findbiggest())])
+            popularplayerCounts[popularplayerCounts.indexOf(findbiggest())] = 0
+            let first = fourmax[0]
+            let second = fourmax[1]
+            let third = fourmax[2]
+            let fourth = fourmax[3]
+            const popularembed = new Discord.MessageEmbed()
+            .setTitle('Here are the current most popular Prodigy worlds.')
+            .setDescription('This lists the four worlds that most people are in.')
+            .addField(':first_place: First:',`${first.name}, with a count of ${first.count} people online.`)
+            .addField(':second_place: Second:',`${second.name}, with a count of ${second.count} people online.`)
+            .addField(':third_place: Third:',`${third.name}, with a count of ${third.count} people online.`)
+            .addField(':medal: Fourth:',`${fourth.name}, with a count of ${fourth.count} people online.`)
+            message.channel.send(popularembed)
+            break;
+            case 'p:unpopular':
+                let unpopularworlds = await (await fetch('https://api.prodigygame.com/multiplayer-api/worlds')).json()
+            let unpopularplayerCounts = []
+
+            for (i = 0; i < unpopularworlds.length; i++) {
+                unpopularplayerCounts.push(unpopularworlds[i].count)
+            }
+            function findsmallest() {
+                let min = unpopularplayerCounts.reduce(function (a, b) {
+                    return Math.min(a, b);
+                })
+                return min;
+            };
+            let fourmin = []
+            fourmin.push(unpopularworlds[unpopularplayerCounts.indexOf(findsmallest())])
+            unpopularplayerCounts[unpopularplayerCounts.indexOf(findsmallest())] = Infinity
+            fourmin.push(unpopularworlds[unpopularplayerCounts.indexOf(findsmallest())])
+            unpopularplayerCounts[unpopularplayerCounts.indexOf(findsmallest())] = Infinity
+            fourmin.push(unpopularworlds[unpopularplayerCounts.indexOf(findsmallest())])
+            unpopularplayerCounts[unpopularplayerCounts.indexOf(findsmallest())] = Infinity
+            fourmin.push(unpopularworlds[unpopularplayerCounts.indexOf(findsmallest())])
+            unpopularplayerCounts[unpopularplayerCounts.indexOf(findsmallest())] = Infinity
+            let minfirst = fourmin[0]
+            let minsecond = fourmin[1]
+            let minthird = fourmin[2]
+            let minfourth = fourmin[3]
+            const unpopularembed = new Discord.MessageEmbed()
+            .setTitle('Here are the current least popular Prodigy worlds.')
+            .setDescription('This lists the four worlds that the least people are in.')
+            .addField('<:downvote:690303949475152004> First:',`${minfirst.name}, with a count of ${minfirst.count} people online.`)
+            .addField(':arrow_down_small: Second:',`${minsecond.name}, with a count of ${minsecond.count} people online.`)
+            .addField(':arrow_down: Third:',`${minthird.name}, with a count of ${minthird.count} people online.`)
+            .addField(':arrow_double_down: Fourth:',`${minfourth.name}, with a count of ${minfourth.count} people online.`)
+            message.channel.send(unpopularembed)
+            break;
+        }
 })
 bot.login(token)
